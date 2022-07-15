bbc77be9133ce450a9b6bada1574a474cf4ebc96
Viktoria <syrcova.viktorija@gmail.com>
2022-07-15 13:37:53 +0300

    keyboard fix

--- src/keyboard.js
+++ src/keyboard.js
@@ -3,7 +3,6 @@
 // Главное не используй всё вместе!
 
 const keypress = require("keypress");
-const redline = require("readline");
 
 // Управление.
 // Настроим соответствия нажатий на клавиши и действий в игре.
@@ -24,14 +23,14 @@ class Keyboard {
 
 function runInteractiveConsole() {
   keypress(process.stdin);
-  process.stdin.on("keypress", (ch, key) => {
+  process.stdin.on('keypress', (ch, key) => {
     if (key) {
       // Вызывает команду, соответствующую нажатой кнопке.
       if (key.name in keyboard) {
-        keyboard[key.name]();
+        this.keyboard[key.name]();
       }
       // Прерывание программы.
-      if (key.ctrl && key.name === "c") {
+      if (key.ctrl && key.name === 'c') {
         process.exit();
       }
     }
